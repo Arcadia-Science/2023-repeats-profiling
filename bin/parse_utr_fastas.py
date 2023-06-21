b69eacf786d73ba8868e73aa7e47505a5e6e19f6
elizabethmcd <elizabethmcd93@gmail.com>
2023-06-21 12:23:11 -0700

    base script for parsing utr fasta results to get tsv of repeat information

--- bin/parse_utr_fastas.py
+++ bin/parse_utr_fastas.py
@@ -13,10 +13,25 @@ args = parser.parse_args()
 input_file = args.input_file
 output_file = args.output_file
 
-# regular expression pattern to extract the repeat sequence and unit information
-pattern = r"<([^>]*)>"
+# regular expression pattern to extract the repeat sequence and unit information from <> and number directly after <>
+pattern = r"<([^>]*)>(\d+)"
 
 # basename of input fasta file to get gene name
 input_basename = os.path.splitext(os.path.basename(input_file))[0]
 
 # open the input FASTA file
+with open(input_file, "r") as fasta_file:
+    # Open the output file in write mode
+    with open(output_file, "w") as tsv_file:
+        # Write the header row
+        tsv_file.write("Accession\tRepeat_Sequence\tRepeat_Unit\n")
+
+        # Iterate over the lines in the file
+        for line in fasta_file:
+            # Check if the line starts with '>'
+            if line.startswith("> #Info"):
+                # Extract repeat and unit information using regex
+                matches = re.findall(pattern, line)
+                if matches:
+                    for repeat, unit in matches:
+                        tsv_file.write(f"{input_basename}\t{repeat}\t{unit}\n")
